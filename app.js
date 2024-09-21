4751d353f17945748420129aa10d528b9b7de295
Delight Bernard <142695317+bernieboy0001@users.noreply.github.com>
2024-09-21 13:56:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('nav-bar');
+const close = document.getElementById('close');
+
+if(bar){
+
+    bar.addEventListener('click', () =>{
+
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
